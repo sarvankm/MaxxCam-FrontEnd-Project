272dae442382febd5c56e12d69e2a8b1ad920843
Sarvan Mammadov <sarvan.m.ov@gmail.com>
2022-04-23 13:49:37 +0400

    grid_layout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import News from './components/News';
 import Footer from './layout/footer/Footer';
 import Header from './layout/header/Header';
-
+import './App.css'
 
 const App = () => {
     return (
-        <>
+        <div className='app'>
             <Header/>
             <News/>
             <Footer/>
-        </>
+        </div>
     );
 }
 
